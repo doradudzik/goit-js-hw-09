d54d898e6128467e844e222a2c8924433f8e22ae
Dorota Dudzik <113298700+doradudzik@users.noreply.github.com>
2023-03-13 12:22:08 +0100

    .

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -43,27 +43,3 @@ function submitForm(event) {
   }
   event.currentTarget.reset();
 }
-// const inputD = document.querySelector("input[name='delay']");
-// const inputS = document.querySelector("input[name='step']");
-// const inputA = document.querySelector("input[name='amount']");
-
-// function submitForm(event) {
-//   event.preventDefault();
-//   for (let i = 0; i < inputA.value; i++) {
-//     let position = i + 1;
-//     let delayStep = 'inputD.value + inputS.value * i';
-//     console.log(delayStep);
-//     createPromise(position, delayStep)
-//       .then(({ position, delay }) => {
-//         Notiflix.Notify.success(
-//           `✅ Fulfilled promise ${position} in ${delay}ms`
-//         );
-//       })
-//       .catch(({ position, delay }) => {
-//         Notiflix.Notify.failure(
-//           `❌ Rejected promise ${position} in ${delay}ms`
-//         );
-//       });
-//   }
-//   event.currentTarget.reset();
-// }
