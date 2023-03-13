b55abf351c6ac58b1b4b36ef8a398ae34e6e8b57
Dorota Dudzik <113298700+doradudzik@users.noreply.github.com>
2023-03-13 12:04:15 +0100

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,26 +1,26 @@
-const startBtn = document.querySelector('button[data-start]');
-const stopBtn = document.querySelector('button[data-stop]');
-const body = document.querySelector('body');
+// const startBtn = document.querySelector('button[data-start]');
+// const stopBtn = document.querySelector('button[data-stop]');
+// const body = document.querySelector('body');
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
 
-let timerId = null;
+// let timerId = null;
 
-startBtn.addEventListener('click', () => {
-  timerId = setInterval(() => {
-    body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
+// startBtn.addEventListener('click', () => {
+//   timerId = setInterval(() => {
+//     body.style.backgroundColor = getRandomHexColor();
+//   }, 1000);
 
-  // Zablokowanie kliknietego startBtn i odblokowanie stopBtn
-  startBtn.disabled = true;
-  stopBtn.disabled = false;
-});
+//   // Zablokowanie kliknietego startBtn i odblokowanie stopBtn
+//   startBtn.disabled = true;
+//   stopBtn.disabled = false;
+// });
 
-stopBtn.addEventListener('click', () => {
-  clearInterval(timerId);
-  // Zablokowanie klikniętego stopBtn i odpblokowanie startBtn
-  startBtn.disabled = false;
-  stopBtn.disabled = true;
-});
+// stopBtn.addEventListener('click', () => {
+//   clearInterval(timerId);
+//   // Zablokowanie klikniętego stopBtn i odpblokowanie startBtn
+//   startBtn.disabled = false;
+//   stopBtn.disabled = true;
+// });
